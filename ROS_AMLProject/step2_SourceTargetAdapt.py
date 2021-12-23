b6d8db081e709eeeac8f0455e3a9e198c8edda15
CoccoCarmiano <matteo.quarta.carmiano@gmail.com>
2021-12-23 21:02:15 +0100

    Diofa

--- ROS_AMLProject/step2_SourceTargetAdapt.py
+++ ROS_AMLProject/step2_SourceTargetAdapt.py
@@ -56,6 +56,7 @@ def _do_epoch(args,feature_extractor,rot_cls,obj_cls,source_loader,target_loader
 
 
 def step2(args,feature_extractor,rot_cls,obj_cls,source_loader,target_loader_train,target_loader_eval,device):
+    prit("Help")
     pass
 #     optimizer, scheduler = get_optim_and_scheduler(feature_extractor,rot_cls,obj_cls, args.epochs_step2, args.learning_rate, args.train_all)
 # 
