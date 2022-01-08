fe91bf3fc2d99794502edf4091e813835a5460ac
Alessandro Versace <aleversace98@gmail.com>
2022-01-08 18:27:45 +0100

    add: step2 fast implementation, must still be tested and finished

--- ROS_AMLProject/step2_SourceTargetAdapt.py
+++ ROS_AMLProject/step2_SourceTargetAdapt.py
@@ -16,6 +16,7 @@ def _do_epoch(args,feature_extractor,rot_cls,obj_cls,source_loader,target_loader
     rot_cls.train()
 
     target_loader_train = cycle(target_loader_train)
+    cls_correct, rot_correct, cls_tot, rot_tot = 0, 0, 0, 0
 
     for it, (data_source, class_l_source, _, _) in enumerate(source_loader):
 
@@ -26,37 +27,71 @@ def _do_epoch(args,feature_extractor,rot_cls,obj_cls,source_loader,target_loader
 
         optimizer.zero_grad()
 
+        feature_extractor_output     = feature_extractor(data_source)
+        feature_extractor_output_target = feature_extractor(data_target)
+        feature_extractor_output_target_rot = feature_extractor(data_target_rot)
 
-        # class_loss = ....
-        # rot_loss = ....
+        obj_cls_output = obj_cls(feature_extractor_output)
+        temp = torch.cat((feature_extractor_output_target, feature_extractor_output_target_rot), dim=1)
+        rot_cls_output = rot_cls(temp) 
 
+        class_loss  = criterion(obj_cls_output, class_l_source)
+        rot_loss    = criterion(rot_cls_output, rot_l_target)
         loss = class_loss + args.weight_RotTask_step2*rot_loss
 
         loss.backward()
 
         optimizer.step()
 
-        # _, cls_pred = ...
-        # _, rot_pred = ...
+        cls_pred = torch.argmax(obj_cls_output, dim=1)
+        cls_correct += (cls_pred == class_l_source).sum().item()
+        rot_pred = torch.argmax(rot_cls_output, dim=1)
+        rot_correct += (rot_pred == rot_l_target).sum().item()
 
-    # acc_cls = ...
-    # acc_rot = ...
+        cls_tot += class_l_source.size(0)
+        rot_tot += rot_l_target.size(0)
+
+    acc_cls = cls_correct / cls_tot * 100
+    acc_rot = rot_correct / rot_tot * 100
 
     print("Class Loss %.4f, Class Accuracy %.4f,Rot Loss %.4f, Rot Accuracy %.4f" % (class_loss.item(), acc_cls, rot_loss.item(), acc_rot))
 
 
     #### Implement the final evaluation step, computing OS*, UNK and HOS
-    # feature_extractor.eval()
-    # obj_cls.eval()
+    feature_extractor.eval()
+    obj_cls.eval()
     # rot_cls.eval()
 
-    # with torch.no_grad():
-    #     for it, (data, class_l,_,_) in enumerate(target_loader_eval):
+    tot_known, tot_unk, OS_, UNK = 0, 0, 0, 0
+
+    with torch.no_grad():
+        for it, (data, class_l,_,_) in enumerate(target_loader_eval):
+            feature_extractor_output  = feature_extractor(data)
+            obj_cls_output = obj_cls(feature_extractor_output)
+
+            cls_pred = torch.argmax(obj_cls_output, dim=1)
+
+            # Are this masks correct? Need to check
+            mask_known = (class_l != args.n_classes_known+1)
+            mask_unk = (class_l == args.n_classes_known+1)
+
+            tot_known = mask_known.sum()
+            tot_unk = mask_unk.sum()
+
+            OS_ += (cls_pred == class_l[mask_known]).sum().item()
+            UNK += (cls_pred == class_l[mask_unk]).sum().item()
+            # HOS = ???
+        
+        OS_ = OS_ / tot_known * 100
+        UNK = UNK / tot_unk * 100
+
+        print(f'\nOS_={OS_}, UNK={UNK}, HOS={...}')
+
 
 
 
 def step2(args,feature_extractor,rot_cls,obj_cls,source_loader,target_loader_train,target_loader_eval,device):
-    prit("Help")
+    print("Help")
     pass
 #     optimizer, scheduler = get_optim_and_scheduler(feature_extractor,rot_cls,obj_cls, args.epochs_step2, args.learning_rate, args.train_all)
 # 
