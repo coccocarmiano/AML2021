6b5c9246a6628f60de04efec9f3e41b31cb02611
Giuseppe Ruggeri <ruggerigiuseppe_work@hotmail.com>
2022-01-31 21:03:09 +0100

    removed center loss from step2

--- ROS_AMLProject/step2_SourceTargetAdapt.py
+++ ROS_AMLProject/step2_SourceTargetAdapt.py
@@ -4,7 +4,6 @@ from optimizer_helper import get_optim_and_scheduler
 
 from itertools import cycle
 from tqdm import tqdm
-from center_loss import CenterLoss
 
 def _do_epoch(args, feature_extractor, rot_cls, obj_cls, source_loader, target_loader_train, target_loader_eval, optimizer, device):
 
@@ -25,10 +24,6 @@ def _do_epoch(args, feature_extractor, rot_cls, obj_cls, source_loader, target_l
     cls_criterion = nn.CrossEntropyLoss(weight=w)
     rot_criterion = nn.CrossEntropyLoss()
 
-    if args.center_loss:
-        center_criterion = CenterLoss(num_classes=4, feat_dim=256, use_gpu=True, device=device)
-        center_optimizer = torch.optim.SGD(center_criterion.parameters(), lr=args.learning_rate_center)
-
     # We iterate over two datasets at once
     target_loader_train = cycle(target_loader_train)
     cls_correct, rot_correct = 0, 0
@@ -39,8 +34,6 @@ def _do_epoch(args, feature_extractor, rot_cls, obj_cls, source_loader, target_l
 
         # Zero-out the gradients
         optimizer.zero_grad()
-        if args.center_loss:
-            center_optimizer.zero_grad()
 
         # Move everything to the used device ( CPU/GPU )
         data_source           = data_source.to(device)
@@ -66,38 +59,15 @@ def _do_epoch(args, feature_extractor, rot_cls, obj_cls, source_loader, target_l
         # For the target image, we want the scores from R2
         # For the center loss version, we need to have both the features coming from the first layer of the discriminator
         # and the output scores coming out from the discriminator
-        if args.center_loss:
-            # 1. For each sample in the batch, get the discriminator features and output scores
-            discriminator_features = []
-            discriminator_scores = []
-            for sample in feature_extractor_output_target_conc:
-                discriminator_sample_scores, discriminator_sample_features = rot_cls(sample)
-                discriminator_features.append(discriminator_sample_features)
-                discriminator_scores.append(discriminator_sample_scores)
-
-            # 2. Transform them back again into tensors (#batch_size, #features_dim)
-            discriminator_features = torch.vstack(discriminator_features)
-            discriminator_scores = torch.vstack(discriminator_scores)
-        else:
-            discriminator_scores = torch.vstack([rot_cls(sample)[0] for sample in feature_extractor_output_target_conc])
+        discriminator_scores = torch.vstack([rot_cls(sample)[0] for sample in feature_extractor_output_target_conc])
 
         # Evaluate losses
         class_loss  = cls_criterion(obj_cls_target_scores, data_source_label)
         rot_loss    = rot_criterion(discriminator_scores, data_target_rot_label) * args.weight_RotTask_step2
-        cent_loss   = .0
 
-        if args.center_loss:
-            cent_loss = center_criterion(discriminator_features, data_target_rot_label) * args.cl_lambda
-
-        loss = class_loss + rot_loss + cent_loss
+        loss = class_loss + rot_loss
         loss.backward()
         optimizer.step()
-
-        # by doing so, cl_lambda would not impact on the learning of centers
-        if args.center_loss:
-            for param in center_criterion.parameters():
-                param.grad.data *= (1. / args.cl_lambda)
-            center_optimizer.step()
 
         cls_pred     = torch.argmax(obj_cls_target_scores, dim=1)
         cls_correct += (cls_pred == data_source_label).sum().item()
@@ -113,8 +83,6 @@ def _do_epoch(args, feature_extractor, rot_cls, obj_cls, source_loader, target_l
     print("Train Dataset Stats:")
     print(f"\tClass  Loss: {class_loss.item():.4f}")
     print(f"\tRot    Loss: {rot_loss.item():.4f}")
-    if args.center_loss:
-        print(f"\tCenter Loss: {cent_loss.item():.4f}")
     print(f"\tRot    Acc.: {acc_rot:.2f}%")
     print(f"\tClass  Acc.: {acc_cls:.2f}%")
 
