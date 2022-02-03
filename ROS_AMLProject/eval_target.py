9fed2716144e4828d8fdd39210c843be1d57fd60
Giuseppe Ruggeri <ruggerigiuseppe_work@hotmail.com>
2022-02-03 19:42:50 +0100

    multihead now use all 180 output to acquire the normality score for the separation

--- ROS_AMLProject/eval_target.py
+++ ROS_AMLProject/eval_target.py
@@ -49,11 +49,11 @@ def target_separation(args, E, C, R, target_loader_eval, device, rand):
 
             # TODO: select only one head or apply the softmax to the whole set of output scores from all the heads?
             # If R1 is multihead, we pick the head corresponding to the inferred label
-            if args.multihead:
-                new_R_scores = torch.zeros((predicted_labels.size()[0], 4))
-                for i, sample_label in enumerate(predicted_labels):
-                    new_R_scores[i] = R_scores[i, sample_label * 4 : sample_label * 4 + 4]
-                R_scores = new_R_scores
+            # if args.multihead:
+            #     new_R_scores = torch.zeros((predicted_labels.size()[0], 4))
+            #     for i, sample_label in enumerate(predicted_labels):
+            #         new_R_scores[i] = R_scores[i, sample_label * 4 : sample_label * 4 + 4]
+            #     R_scores = new_R_scores
 
             # Compute softmax and get the maximum probability as the normality score
             R_probabilities = softmax(R_scores)
