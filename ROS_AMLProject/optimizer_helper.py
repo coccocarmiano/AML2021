0375644a10b3f253500495b20e9d5a306ca7c18c
Giuseppe Ruggeri <ruggerigiuseppe_work@hotmail.com>
2022-02-06 19:07:06 +0100

    deactivated scheduling

--- ROS_AMLProject/optimizer_helper.py
+++ ROS_AMLProject/optimizer_helper.py
@@ -24,7 +24,7 @@ def get_optim_and_scheduler(E, C, R, epochs, lr, train_all):
 def get_optim_scheduler_loss_center_loss(lr, epochs, device):
     criterion_center = CenterLoss(num_classes=4, feat_dim=256, use_gpu=True, device=device)
     optimizer_center = optim.SGD(criterion_center.parameters(), weight_decay=.0005, momentum=.9, lr=lr)
-    step_size = int(epochs * .8)
+    step_size = int(epochs * 10)
     scheduler = optim.lr_scheduler.StepLR(optimizer_center, step_size=step_size)
 
     return optimizer_center, scheduler, criterion_center
