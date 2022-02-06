a4c73000db9022cd675508b1b908f1358786ad99
Giuseppe Ruggeri <ruggerigiuseppe_work@hotmail.com>
2022-02-06 20:05:31 +0100

    deactivated scheduler

--- ROS_AMLProject/optimizer_helper.py
+++ ROS_AMLProject/optimizer_helper.py
@@ -16,7 +16,7 @@ def get_optim_and_scheduler(E, C, R, epochs, lr, train_all):
         params = list(C.parameters()) + R_params
         optimizer = optim.SGD(params, weight_decay=.0005, momentum=.9, lr=lr)
 
-    step_size = int(epochs * .8)
+    step_size = int(epochs * 10)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=step_size)
 
     return optimizer, scheduler
