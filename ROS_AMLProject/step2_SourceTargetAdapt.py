216aa6fc1ecfbce06a4555201170cfceb3860e7d
Giuseppe Ruggeri <ruggerigiuseppe_work@hotmail.com>
2022-02-06 18:16:43 +0100

    updated unk rebalance

--- ROS_AMLProject/step2_SourceTargetAdapt.py
+++ ROS_AMLProject/step2_SourceTargetAdapt.py
@@ -9,7 +9,7 @@ def _do_epoch(args, E, C, R, source_loader, target_loader_train, optimizer, devi
 
     # Adjust the learning rate for the unknown class
     w = [ 1.0 for _ in range(args.n_classes_known + 1) ]
-    w[-1] = 1/45
+    w[-1] = 1/20
     w = torch.tensor(w).to(device)
 
     C_criterion = nn.CrossEntropyLoss(weight=w)
