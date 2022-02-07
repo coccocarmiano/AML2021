8aeb09a2d42123ae8f6f7c4f05ab0d22bcd1432b
AleVersace <aleversace98@gmail.com>
2022-02-07 13:39:46 +0100

    add: optimizer step

--- ROS_AMLProject/optimizer_helper.py
+++ ROS_AMLProject/optimizer_helper.py
@@ -16,7 +16,7 @@ def get_optim_and_scheduler(E, C, R, epochs, lr, train_all):
         params = list(C.parameters()) + R_params
         optimizer = optim.SGD(params, weight_decay=.0005, momentum=.9, lr=lr)
 
-    step_size = int(epochs * 10)
+    step_size = int(epochs * 0.9)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=step_size)
 
     return optimizer, scheduler
@@ -24,7 +24,7 @@ def get_optim_and_scheduler(E, C, R, epochs, lr, train_all):
 def get_optim_scheduler_loss_center_loss(lr, epochs, device):
     criterion_center = CenterLoss(num_classes=4, feat_dim=256, use_gpu=True, device=device)
     optimizer_center = optim.SGD(criterion_center.parameters(), weight_decay=.0005, momentum=.9, lr=lr)
-    step_size = int(epochs * 10)
+    step_size = int(epochs * 0.9)
     scheduler = optim.lr_scheduler.StepLR(optimizer_center, step_size=step_size)
 
     return optimizer_center, scheduler, criterion_center
